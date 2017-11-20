974c153782f06e0eea5814f1f8311428f68d4c42
wmoecke <wmoecke@gmail.com>
2017-11-20 17:43:22 -0200

    Added new images to the slider

--- js/index.js
+++ js/index.js
@@ -1,35 +1,34 @@
 $(function() {
 	$("#slide img:eq(0)").addClass("ativo").show();
-	var img = $("#slide img.ativo");
-	var slideWidth = parseInt($("#slide").css("width"));
-	var slideHeight = parseInt($("#slide").css("height"));
-	var imgWidth = parseInt(img.css("width"));
-	var imgHeight = parseInt(img.css("height"));
-	if(imgWidth < $("#slide").css("width"))
-		img.css("left", ($("#slide").css("width") - imgWidth) / 2);
-	if(imgHeight < $("#slide").css("height"))
-		img.css("top", ($("#slide").css("height") - imgHeight) / 2);
-
+	computeSlideDimensions();
 	var texto = $(".ativo").attr("data-description");
 	$("#slide").prepend("<p>" + texto + "</p>");
 	setInterval(slide, 5000);
 	
 	$('li').click(function() {
 		// custom handling here
-		var src = $(this).attr("data-href"); //$(e).prop("data-href");
+		var src = $(this).attr("data-href");
 		window.location.href = src;
+		// attempt to load the contents of the links into an element on our page;
+		// not really good, as modern browsers inhibit cross-domain page loading.
 		//src = "https://cors-anywhere.herokuapp.com/" + src;
 		//$('#showContent').load(src);
 	});
 });
 
 function slide(){
-	if($(".ativo").next().size()){
+	if($(".ativo").next().size()) {
 		$(".ativo").fadeOut("slow").removeClass("ativo").next().fadeIn("fast").addClass("ativo");
 	} else {
 		$(".ativo").fadeOut().removeClass("ativo");
 		$("#slide img:eq(0)").fadeIn("fast").addClass("ativo");
 	}
+	computeSlideDimensions();
+	var texto = $(".ativo").attr("data-description");
+	$("#slide p").hide().html(texto).delay(500).fadeIn("fast");
+}
+
+function computeSlideDimensions() {
 	var slide = $("#slide");
 	var img = $("#slide img.ativo");
 	var slideWidth = parseInt(slide.css("width"));
@@ -40,6 +39,4 @@ function slide(){
 		img.css("left", (slideWidth - imgWidth) / 2);
 	if(imgHeight < slideHeight)
 		img.css("top", (slideHeight - imgHeight) / 2);
-	var texto = $(".ativo").attr("data-description");
-	$("#slide p").hide().html(texto).delay(500).fadeIn("fast");
 }
